d361de42106ef903291d52d925c01bbd4acdd3a0
Chris <dribbleribbletribble@gmail.com>
2018-10-31 16:27:27 -0400

    Update
    Update

--- WoW Lua Addon/Pixels/Pixels.lua
+++ WoW Lua Addon/Pixels/Pixels.lua
@@ -10,12 +10,34 @@ nextSpellFrameTexture:SetAllPoints()
 function buffcount(unit, buff)
     for i=1,40 do
         local name, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, 
-        nameplateShowPersonal, spellId, canApplyAura, isBossDebuff, nameplateShowAll, timeMod, value1, value2, value3 = UnitBuff("player",i)
+        nameplateShowPersonal, spellId, canApplyAura, isBossDebuff, nameplateShowAll, timeMod, value1, value2, value3 = UnitBuff(unit,i)
         if name == buff then
             return (count == 0 and 1 or count)
         end
     end
     return 0
+end
+
+function debuffcount(unit, buff)
+    for i=1,40 do
+        local name, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, 
+        nameplateShowPersonal, spellId, canApplyAura, isBossDebuff, nameplateShowAll, timeMod, value1, value2, value3 = UnitDebuff(unit,i)
+        if name == buff then
+            return (count == 0 and 1 or count)
+        end
+    end
+    return 0
+end
+
+function auraexists(unit, auraName)
+    local name, icon, count, debuffType, duration, expirationTime, unitCaster, canStealOrPurge, 
+    nameplateShowPersonal, spellId, canApplyAura, isBossDebuff, isCastByPlayer, nameplateShowAll, timeMod = AuraUtil.FindAuraByName(auraName, unit, "player")
+		
+	if name then
+        return true
+    else
+		return false
+	end
 end
 
 function spellcooldown(spell)
@@ -31,6 +53,15 @@ function spellcooldown(spell)
     end
 end
 
+function interruptable(unit) 
+    local name, subText, text, texture, startTime, endTime, isTradeSkill, castID, notInterruptible = UnitCastingInfo(unit)
+    if notInterruptible == 1 then
+        return true
+    else 
+        return false
+    end
+end
+
 
 function setNextSpell(r,g,b,a)     
     nextSpellFrameTexture:SetColorTexture(r,g,b,a)
@@ -39,23 +70,26 @@ end
 
 
 function loop:onUpdate()
-    if not UnitExists("target") then
+    if not UnitExists("target") or UnitIsDeadOrGhost("target") or UnitIsFriend("player", "target") then
         return setNextSpell(0,0,0,0)
     end
-    if spellcooldown("Spirit Bomb") == 0 and buffcount("player", "Soul Fragments") >= 3 then
-        return setNextSpell(.5,0,0,1)
+    if spellcooldown("Disrupt") == 0 and interruptable("target") then
+        return setNextSpell(0,.5,1,1)
     end
-
-    if spellcooldown("Fracture") == 0 and IsActionInRange(2, "target") then
-        return setNextSpell(0,.5,0,1)
-    end
-
     if spellcooldown("Immolation Aura") == 0 then
         return setNextSpell(0,0,.5,1)
     end
-
-    if spellcooldown("Soul Cleave") == 0 and IsActionInRange(4, "target") then
+    if spellcooldown("Soul Cleave") == 0 and UnitPower("player", 18) >= 40 and IsActionInRange(4, "target") and debuffcount("target", "Frailty") >= 1 then
         return setNextSpell(1,1,0,1)
+    end
+    if spellcooldown("Spirit Bomb") == 0 and UnitPower("player", 18) >= 40 and buffcount("player", "Soul Fragments") >= 3 and debuffcount("target", "Frailty") == 0 then
+        return setNextSpell(.5,0,0,1)
+    end
+    if spellcooldown("Fracture") == 0 and IsActionInRange(2, "target") then
+        return setNextSpell(0,.5,0,1)
+    end
+    if spellcooldown("Throw Glaive") == 0 and IsActionInRange(3, "target") then
+        return setNextSpell(0,1,1,1)
     end
 end
 
